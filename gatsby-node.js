2ace1d10c78c57338b575d90cd9e0d6aeb0807b3
Eric Smith <ericsmith.100@gmail.com>
2020-07-10 10:10:28 -0400

    Add gatsby node data for GQL query fetching

---
+++ gatsby-node.js
@@ -0,0 +1,33 @@
+const axios = require('axios')
+
+exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }) => {
+    //todo put bizID inside of gatsby-config and pull it from there via option parameters
+  const bizID = 'bzn-yO3xgUsKQCS7GWg0Q2ewbQ'
+  const { createNode } = actions
+
+  async function getMetaData(id) {
+    const data = await axios.get(
+      `https://data.prod.gonation.com/profile/getname/?profile_id=${id}`
+    )
+    return data
+  }
+
+  const data = await getMetaData(bizID)
+
+  const nodeContent = JSON.stringify(data.data)
+
+  const nodeMeta = {
+    id: createNodeId(`metaData-${bizID}`),
+    parent: null,
+    children: [],
+    internal: {
+      type: `SiteMetaData`,
+      mediaType: `text/html`,
+      content: nodeContent,
+      contentDigest: createContentDigest(data.data),
+    },
+  }
+
+  const node = Object.assign({}, data, nodeMeta)
+  createNode(node)
+}
