26aedc81cc2e18ebe0dd0cb928b55808a0a2c877
Eric Smith <ericsmith.100@gmail.com>
2020-07-13 12:59:30 -0400

    Add gallery page layout

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,10 +1,79 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from "../components/Layout"
 import Gallery from "../components/gallery/Gallery"
-export default function gallery() {
+export default function GalleryPage({ data }) {
+  const [navIsOpen, setNavIsOpen] = useState(false)
+  const siteData = data.siteMetaData.data
+  const bizID = data.siteMetaData.bizID
   return (
-    <Layout pageTitle="Gallery">
+    <Layout pageTitle="Gallery" data={siteData} navIsOpen={navIsOpen}>
       <Gallery />
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    siteMetaData {
+      data {
+        avatar {
+          imageBaseUrl
+        }
+        city
+        desc
+        cover {
+          imageBaseUrl
+        }
+        hours {
+          fri {
+            close
+            open
+          }
+          mon {
+            close
+            open
+          }
+          sat {
+            close
+            open
+          }
+          sun {
+            close
+            open
+          }
+          thu {
+            close
+            open
+          }
+          tue {
+            close
+            open
+          }
+          wed {
+            close
+            open
+          }
+        }
+        lastPricelistUpdate {
+          sec
+          usec
+        }
+        links {
+          facebook
+          instagram
+          twitter
+          website
+          youtube
+        }
+        loc
+        slug
+        name
+        phone
+        state
+        street
+        zip
+      }
+      bizID
+    }
+  }
+`
