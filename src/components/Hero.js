62113f8f004c3061c321b3ded298d680ac50babf
stefantrinh1 <stefantrinh1.work@gmail.com>
2020-07-15 11:05:59 -0600

    converting to styled components

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,34 +1,55 @@
-import React from 'react';
-import slugify from 'slugify'
+import React from "react"
+import styled from "styled-components"
+import slugify from "slugify"
 
-const Hero = ({pageTitle}) => {
-
-    const getLink = () => {
-        switch(pageTitle) {
-            case 'food': 
-                return `url(https://res.cloudinary.com/gonation/image/upload/w_2000/v1594395080/sites/dockside-brewing/food-hero.jpg)`
-            case 'faq': 
-                return `url(https://res.cloudinary.com/gonation/image/upload/v1594397872/sites/dockside-brewing/faq-hero.jpg)`
-        }
+const Hero = ({ pageTitle }) => {
+  const getLink = () => {
+    switch (pageTitle) {
+      case "food":
+        return `url(https://res.cloudinary.com/gonation/image/upload/w_2000/v1594395080/sites/dockside-brewing/food-hero.jpg)`
+      case "faq":
+        return `url(https://res.cloudinary.com/gonation/image/upload/v1594397872/sites/dockside-brewing/faq-hero.jpg)`
     }
+  }
 
-    const heroStyle = {
-      //   background: `url(https://res.cloudinary.com/gonation/image/upload/v1/sites/dockside-brewing/${slugify(
-      //     pageTitle,
-      //     { lower: true }
-      //   )}-hero.jpg)`,
-      background: `url(https://res.cloudinary.com/gonation/image/upload/v1/sites/dockside-brewing/${pageTitle}-hero.jpg)`,
-      width: "100%",
-      backgroundPosition: "center",
-      backgroundSize: "cover",
-    }
-     return (
-      <section className="section section__hero" style={heroStyle}>
-        <div className="container">
-          <h1>{pageTitle}</h1>
-        </div>
-      </section>
-    )
+  return (
+    <HeroContainer
+      backgroundImage={`url(https://res.cloudinary.com/gonation/image/upload/v1/sites/dockside-brewing/${pageTitle}-hero.jpg)`}
+    >
+      <div className="container">
+        <PageTitle>{pageTitle}</PageTitle>
+      </div>
+    </HeroContainer>
+  )
 }
 
-export default Hero;
+const HeroContainer = styled.section`
+  height: 500px;
+  display: flex;
+  width: 100%;
+  background-position: center;
+  background-size: cover;
+  justify-content: center;
+  align-items: flex-end;
+  padding: 3rem 1.5rem;
+  width: 100%;
+  background-image: ${props =>
+    props.backgroundImage ? props.backgroundImage : ""};
+  background-color: ${props => (!props.backgroundImage ? "lightgrey" : "")};
+  @media min-width(1024px) {
+    height: 700px;
+  }
+`
+
+const PageTitle = styled.h1`
+  color: white;
+  text-align: center;
+  font-size: 2.5rem;
+  font-weight: bold;
+  text-transform: uppercase;
+  @media min-width(1024px) {
+    font-size: 3.5rem;
+  }
+`
+
+export default Hero
--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,99 +1,177 @@
-import React from 'react';
-import Layout from '../components/Layout'
-import FAQ from '../components/icons/BoatIcon'
-import FAQBlock from '../components/FAQBlock'
-import Faq from 'react-faq-component';
+import React from "react"
+import styled from "styled-components"
+import Layout from "../components/Layout"
+import FAQ from "../components/icons/BoatIcon"
+import FAQBlock from "../components/FAQBlock"
+import Faq from "react-faq-component"
 
-const faq = ({data}) => {
-    const siteData = data.siteMetaData
-    const bizID = data.siteMetaData.bizID
+const faq = ({ data }) => {
+  const siteData = data.siteMetaData
+  const bizID = data.siteMetaData.bizID
 
-    const faqData = {
-      title: "Our frequently asked questions",
-      rows: [
-        {
-          title: (
-            <div class="columns is-mobile columns__faq is-vcentered">
-              <div className="column is-1">
-                <FAQ width="50px" />
-              </div>
-              <div className="column column__question">
-                <p>Question Here</p>
-              </div>
+  const faqData = {
+    title: "Our frequently asked questions",
+    rows: [
+      {
+        title: (
+          <div class="columns is-mobile columns__faq is-vcentered">
+            <div className="column is-1">
+              <FAQ width="50px" />
             </div>
-          ),
-          content: "Lorem ipsum dolor sit amet, consectetur ",
-        },
-        {
-          title: (
-            <div class="columns is-mobile columns__faq is-vcentered">
-              <div className="column is-1">
-                <FAQ width="50px" />
-              </div>
-              <div className="column column__question">
-                <p>Question Here</p>
-              </div>
+            <div className="column column__question">
+              <p>Question Here</p>
             </div>
-          ),
-          content: "Lorem ipsum dolor sit amet, consectetur ",
-        },
-        {
-          title: (
-            <div class="columns is-mobile columns__faq is-vcentered">
-              <div className="column is-1">
-                <FAQ width="50px" />
-              </div>
-              <div className="column column__question">
-                <p>Question Here</p>
-              </div>
+          </div>
+        ),
+        content: "Lorem ipsum dolor sit amet, consectetur ",
+      },
+      {
+        title: (
+          <div class="columns is-mobile columns__faq is-vcentered">
+            <div className="column is-1">
+              <FAQ width="50px" />
             </div>
-          ),
-          content: "Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur ",
-        },
-        {
-          title: (
-            <div class="columns is-mobile columns__faq is-vcentered">
-              <div className="column is-1">
-                <FAQ width="50px" />
-              </div>
-              <div className="column column__question">
-                <p>Question Here</p>
-              </div>
+            <div className="column column__question">
+              <p>Question Here</p>
             </div>
-          ),
-          content: "Lorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consectetur ",
-        },
-        {
-          title: (
-            <div class="columns is-mobile columns__faq is-vcentered">
-              <div className="column is-1">
-                <FAQ width="50px" />
-              </div>
-              <div className="column column__question">
-                <p>Question Here</p>
-              </div>
+          </div>
+        ),
+        content: "Lorem ipsum dolor sit amet, consectetur ",
+      },
+      {
+        title: (
+          <div class="columns is-mobile columns__faq is-vcentered">
+            <div className="column is-1">
+              <FAQ width="50px" />
             </div>
-          ),
-          content: "Lorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consectetur ",
-        },
-      ],
-    }
+            <div className="column column__question">
+              <p>Question Here</p>
+            </div>
+          </div>
+        ),
+        content:
+          "Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur ",
+      },
+      {
+        title: (
+          <div class="columns is-mobile columns__faq is-vcentered">
+            <div className="column is-1">
+              <FAQ width="50px" />
+            </div>
+            <div className="column column__question">
+              <p>Question Here</p>
+            </div>
+          </div>
+        ),
+        content:
+          "Lorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consectetur ",
+      },
+      {
+        title: (
+          <div class="columns is-mobile columns__faq is-vcentered">
+            <div className="column is-1">
+              <FAQ width="50px" />
+            </div>
+            <div className="column column__question">
+              <p>Question Here</p>
+            </div>
+          </div>
+        ),
+        content:
+          "Lorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consectetur ",
+      },
+    ],
+  }
 
-
-    return (
-        <Layout pageTitle="FAQ" >
-          <section className="section section__faq">
-              <div className="container container__faq">
-                  <Faq data={faqData} />
-              </div>
-          </section>
-        </Layout>
-    );
+  return (
+    <Layout pageTitle="FAQ">
+      <Page>
+        <ContentContainer className="container">
+          <Faq data={faqData} />
+        </ContentContainer>
+      </Page>
+    </Layout>
+  )
 }
 
-export default faq;
+export default faq
 
+const Page = styled.section`
+  padding: 3rem 1.5rem;
+  background: #eee;
+`
 
+// the faq library doesn't play well with styled components. added css to container
+const ContentContainer = styled.div`
+  max-width: 800px;
+
+  .styles_faq-row-wrapper__3Hsch {
+    padding: 1rem;
+  }
+
+  .section__faq {
+    background: #eee;
+  }
+
+  .styles_faq-row-wrapper__3Hsch h2 {
+    text-align: center;
+    font-size: 2rem;
+    text-transform: uppercase;
+  }
+
+  #gatsby-focus-wrapper
+    > main
+    > section.section.section__faq
+    > div
+    > div
+    > section.faq-title.styles_faq-row__2Rd2Y {
+    justify-content: center;
+  }
+
+  .styles_faq-row-wrapper__3Hsch
+    .styles_row-body__8wIE9
+    .styles_faq-row__2Rd2Y
+    .styles_row-title__2yqnt
+    > div {
+    width: 100%;
+  }
+
+  .styles_faq-row-wrapper__3Hsch
+    .styles_row-body__8wIE9
+    .styles_faq-row__2Rd2Y
+    .styles_row-title__2yqnt.styles_expanded__2KFpr
+    + .styles_row-content__TVd0Y.styles_animate__3cloa {
+    padding-left: 63px;
+  }
+  .styles_faq-row-wrapper__3Hsch
+    .styles_row-body__8wIE9
+    .styles_faq-row__2Rd2Y
+    .styles_row-title__2yqnt {
+    padding-bottom: 0 !important;
+  }
+
+  .styles_faq-row-wrapper__3Hsch .styles_faq-row__2Rd2Y {
+    padding-bottom: 1rem !important;
+  }
+
+  .styles_faq-row-wrapper__3Hsch
+    .styles_row-body__8wIE9
+    .styles_faq-row__2Rd2Y
+    .styles_row-title__2yqnt.styles_expanded__2KFpr
+    + .styles_row-content__TVd0Y.styles_animate__3cloa {
+    color: $dark;
+    font-weight: 400 !important;
+    padding-right: 6rem !important;
+  }
+
+  .styles_faq-row-wrapper__3Hsch
+    .styles_row-body__8wIE9
+    .styles_faq-row__2Rd2Y
+    .styles_row-title__2yqnt.styles_expanded__2KFpr
+    + .styles_row-content__TVd0Y.styles_animate__3cloa {
+    margin-top: -12px;
+  }
+`
 
 export const query = graphql`
   {
@@ -143,7 +221,7 @@ export const query = graphql`
       links {
         facebook
         instagram
-        twitter 
+        twitter
         youtube
         website
       }
@@ -158,5 +236,3 @@ export const query = graphql`
     }
   }
 `
-
-
